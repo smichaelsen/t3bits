01f42801e35e2ca42b637505ac3e56cefa04e3cf
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2014-01-08 09:43:39 +0100

    Cleanup and show contributors avatars

--- Classes/Podlove/Modules/Contributors/Shortcodes.php
+++ Classes/Podlove/Modules/Contributors/Shortcodes.php
@@ -5,18 +5,6 @@ use \Podlove\Model;
 
 class Shortcodes extends \Podlove\Modules\Contributors\Shortcodes {
 
-	/**
-	 * List of contributions to be rendered.
-	 */
-	private $contributions = array();
-
-	private $id;
-
-	/**
-	 * Shortcode settings.
-	 */
-	private $settings = array();
-
 	public function __construct() {
 		remove_shortcode('podlove-contributors');
 		remove_shortcode('podlove-contributor-list');
@@ -24,24 +12,6 @@ class Shortcodes extends \Podlove\Modules\Contributors\Shortcodes {
 	}
 
 	/**
-	 * Parameters:
-	 *
-	 *	preset      - One of 'table', 'list', 'comma separated'. Default: 'table'
-	 *	title       - Optional table header title. Default: none
-	 *	avatars     - One of 'yes', 'no'. Display avatars. Default: 'yes'
-	 *	role        - Filter lists by role. Default: 'all'
-	 *	roles       - One of 'yes', 'no'. Display role. Default: 'no'
-	 *	group       - Filter lists by group. Default: 'all'
-	 *	groups      - One of 'yes', 'no'. Display group. Default: 'no'
-	 *	donations   - One of 'yes', 'no'. Display donation column. Default: 'no'
-	 *	flattr      - One of 'yes', 'no'. Display Flattr column. Default: 'yes'
-	 *	linkto      - One of 'none', 'publicemail', 'www', 'adn', 'twitter', 'facebook', 'amazonwishlist'.
-	 *	              Links contributor name to the service if available. Default: 'none'
-	 *
-	 * Examples:
-	 *
-	 *	[podlove-contributor-list]
-	 *
 	 * @return string
 	 */
 	public function podlove_contributor_list($attributes) {
@@ -58,7 +28,7 @@ class Shortcodes extends \Podlove\Modules\Contributors\Shortcodes {
 			/** @var \Podlove\Modules\Contributors\Model\Contributor $contributor */
 			$contributor = $contribution->getContributor();
 			$list[] = '<span>'
-				. (($this->settings['avatars'] == 'yes') ? '<span class="avatar">' . $contributor->getAvatar(18) . '</span>' : '')
+				. '<span class="avatar">' . $contributor->getAvatar(18) . '</span>'
 				. ' <span class="name">' . $this->wrapWithLink($contributor, $contributor->getName()) . '</span>'
 				. '</span>';
 		}
