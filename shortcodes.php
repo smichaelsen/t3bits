90a0c82468120ae45d260d99dbd66544b7597ac6
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2014-03-09 13:45:23 +0100

    Hotfix: Remove overwriting of Podlove's Shortcodes because it fatals with the latest Version.

--- shortcodes.php
+++ shortcodes.php
@@ -1,10 +1,10 @@
 <?php
 
-require( get_template_directory() . '/Classes/Podlove/Modules/Contributors/Shortcodes.php' );
+/*require( get_template_directory() . '/Classes/Podlove/Modules/Contributors/Shortcodes.php' );
 
 function t3bits_overwrite_shortcodes() {
 	new \T3Bits\Podlove\Modules\Contributors\Shortcodes();
 }
-add_action( 'after_setup_theme', 't3bits_overwrite_shortcodes' );
+add_action( 'after_setup_theme', 't3bits_overwrite_shortcodes' );*/
 
 ?>
